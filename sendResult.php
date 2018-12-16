2ba8640b27d806ec92afeee3525f91317139a998
Alexey Yurasov <format37@gmail.com>
2018-12-16 17:21:23 +0300

    Create sendResult.php

---
+++ sendResult.php
@@ -0,0 +1,15 @@
+<?php
+unlink($_GET['content']);
+unlink($_GET['style']);
+$token='pasteTokenThere';
+$botUrl = "https://api.telegram.org/bot".$token."/sendPhoto?chat_id=".$_GET['user'];
+$resultUrl='scriptlab.hopto.org/tensorflow-style-transfer/'.$_GET['result'];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+curl_setopt($ch, CURLOPT_URL, $botUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, array("photo" => "@".$resultUrl,));
+$output = curl_exec($ch);
+print $output;
+file_get_contents("http://scriptlab.net/telegram/bots/relaybot/relayPhoto.php?user=-370599054&photoUrl=$resultUrl");
+?>
